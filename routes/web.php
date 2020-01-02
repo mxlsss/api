97327828d49165b2cde7f69aea6d964d2516b454
mxl <1113943731@qq.com>
2020-01-02 00:16:00 +0000

    支付宝

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('alipay','AlipayController@Alipay');  // 发起支付请求
 Route::any('notify','AlipayController@AliPayNotify'); //服务器异步通知页面路径
 Route::any('return','AlipayController@AliPayReturn');  //页面跳转同步通知页面路径
 
+
+
+
+Route::get('/test/pay','Alipay\TestController@alipay');        //去支付
+Route::get('/test/alipay/return','Alipay\PayController@aliReturn');
+Route::post('/test/alipay/notify','Alipay\PayController@notify');
+
